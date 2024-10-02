42245eaa3efaca0d7a8ffeb2abb88c125bd45f7b
niiwiicamo <mail@nilsblume.de>
2024-10-02 16:16:05 +0200

    fix wildcard handling

--- auth-hook.sh
+++ auth-hook.sh
@@ -5,6 +5,10 @@ apk --no-cache add curl
 
 ACME_PREFIX="_acme-challenge"
 
+# check if wildcard cert is requested, as that needs to be stripped
+if [[ "${DDNS_HOST}" == '*.'* ]]; then DDNS_HOST=${DDNS_HOST#\*\.}; fi
+
+# prepend acme prefix to host part
 if [ -z "${DDNS_HOST}" ]; then DDNS_HOST=${ACME_PREFIX}; else DDNS_HOST=${ACME_PREFIX}.${DDNS_HOST}; fi
 
 PAYLOAD="force=true&user=${DDNS_USER}&password=${DDNS_PASS}&txt=${CERTBOT_VALIDATION}&domain=${DDNS_DOMAIN}&host=${DDNS_HOST}&create=true"
